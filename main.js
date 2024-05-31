4ea3f636d3ca73c7b6d62dde8529eea72d3bb44d
Farre <farrezzz@gmail.com>
2024-05-31 23:51:11 +0200

    git init

---
+++ main.js
@@ -0,0 +1,43 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      //fetch Data som sparas i en array.
+      employee: [],
+      //Hålla koll på vilken sida man är på
+      currentPage: 1,
+      //antal sidor. (totalt 2 sidor)
+      totalPages: 2,
+    };
+  },
+  //Logik delen
+  methods: {
+    //Fetchar data och lägger till i employee-array:n
+    displayUsers(page = 1) {
+      fetch(`https://reqres.in/api/users?page=${page}`)
+        .then((result) => result.json())
+        .then((data) => {
+          this.employee = data.data;
+        });
+    },
+    //function för next-knapp
+    displayNextEmployee() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+        this.DisplayUsers(this.currentPage);
+      }
+    },
+    //function för previous-knapp
+    displayPreviousEmployee() {
+      if (this.currentPage >= this.totalPages) {
+        this.currentPage--;
+        this.DisplayUsers(this.currentPage);
+      }
+    },
+  },
+  //
+  mounted() {
+    this.displayUsers();
+  },
+}).mount("#app");
